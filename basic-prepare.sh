b31602e20dd95900f4e185f3a2bf616ee4303976
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 20:36:36 +0300

    Update basic-prepare.sh

--- basic-prepare.sh
+++ basic-prepare.sh
@@ -8,9 +8,11 @@ BLUE='\e[34m'
 RESET='\e[0m'
 
 #Update and upgrade new system
-#sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
+clear
+echo -e "${GREEN}[OK]${RESET} System was sucsessfully updated"
 
-PORT="10122" #Default value of OpenSSH port
+SSHPORT="10122" #Default new value of OpenSSH port
 
 #Parsing cycle
 while [[ $# -gt 0 ]]; do
@@ -28,36 +30,28 @@ while [[ $# -gt 0 ]]; do
 done
 
 #Change default OpenSSH port to custom 10122 port-ssh
-FILE="/etc/ssh/sshd_config"
-sed -i "s|^#\?Port .*$|Port ${PORT}|" "$FILE"
+sed -i "s|^#\?Port .*$|Port ${SSHPORT}|" \ /etc/ssh/sshd_config
 
 #Reload daemon to activate new SSH port
 sudo systemctl daemon-reload && sudo systemctl restart ssh
 
 #Show current listening ports
-clear
-echo -e "Default OpenSSH port: 22 was changed to $PORT"
-echo -e "Listening ports are listed below:"
-ss -ntpl
+echo -e "${GREEN}[OK]${RESET} Default OpenSSH port was changed to $SSHPORT"
+#echo -e "Listening ports are listed below:"
+#ss -ntpl
 
-#Disable ssh-password authentification
 #Check if personal ssh-key avaliable on vm, else exit script
 if [[ ! -s /root/.ssh/authorized_keys ]]; then
     echo -e "\e[31mNo SSH key found for root. Aborting.\e[0m"
     exit 1
 fi
 
-sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" \
-    /etc/ssh/sshd_config
-#FILE="/etc/ssh/sshd_config"
-#sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" "$FILE"
+sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" \ /etc/ssh/sshd_config
 
+#Check if the file even exist
 if [[ -f /etc/ssh/sshd_config.d/50-cloud-init.conf ]]; then
-    sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" \
-        /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" \ /etc/ssh/sshd_config.d/50-cloud-init.conf
 fi
-#FILE="/etc/ssh/sshd_config.d/50-cloud-init.conf"
-#sed -i "s|^#\?PasswordAuthentication .*$|PasswordAuthentication no|" "$FILE"
 
 #restart systemclt daemon to apply changes
 if sshd -t; then
@@ -67,13 +61,41 @@ else
     exit 1
 fi
 
-#sudo systemctl daemon-reload && sudo systemctl restart ssh
-#echo -e "SSH-password authentification was disabled"
+echo -e "${GREEN}[OK]${RESET} SSH-password authentification was disabled"
 
 
 
+UFW_RULES_FILE="/etc/ufw/before.rules"
 
+# Change ACCEPT to DROP in icmp code for INPUT
+sed -i 's/-A ufw-before-input -p icmp --icmp-type destination-unreachable -j ACCEPT/-A ufw-before-input -p icmp --icmp-type destination-unreachable -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-input -p icmp --icmp-type time-exceeded -j ACCEPT/-A ufw-before-input -p icmp --icmp-type time-exceeded -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-input -p icmp --icmp-type parameter-problem -j ACCEPT/-A ufw-before-input -p icmp --icmp-type parameter-problem -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/' "$UFW_RULES_FILE"
 
+#Append a new string to rules
+sed -i '/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/a -A ufw-before-input -p icmp --icmp-type source-quench -j DROP' "$UFW_RULES_FILE"
+
+# Change ACCEPT to DROP in icmp code for FORWARD
+sed -i 's/-A ufw-before-forward -p icmp --icmp-type destination-unreachable -j ACCEPT/-A ufw-before-forward -p icmp --icmp-type destination-unreachable -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-forward -p icmp --icmp-type time-exceeded -j ACCEPT/-A ufw-before-forward -p icmp --icmp-type time-exceeded -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-forward -p icmp --icmp-type parameter-problem -j ACCEPT/-A ufw-before-forward -p icmp --icmp-type parameter-problem -j DROP/' "$UFW_RULES_FILE"
+sed -i 's/-A ufw-before-forward -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-forward -p icmp --icmp-type echo-request -j DROP/' "$UFW_RULES_FILE"
+
+echo -e "${GREEN}[OK]${RESET} Server ping was disabled"
+
+#Setting up an ufw
+sudo ufw default deny incoming > /dev/null 2>&1
+sudo ufw default allow outgoing > /dev/null 2>&1
+
+sudo ufw allow 80/tcp > /dev/null 2>&1
+sudo ufw allow 443/tcp > /dev/null 2>&1
+sudo ufw allow 443/udp > /dev/null 2>&1
+sudo ufw allow $SSHPORT/tcp > /dev/null 2>&1
+
+sudo ufw --force enable > /dev/null 2>&1
+
+echo -e "${GREEN}[OK] ${RESET} Firewall was configured and enabled\n"
 
 
 
