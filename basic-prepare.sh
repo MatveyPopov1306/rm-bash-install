9649aae48eaffe09640beb39703fd329b332a3a2
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 16:47:41 +0300

    Update basic-prepare.sh

--- basic-prepare.sh
+++ basic-prepare.sh
@@ -3,6 +3,23 @@
 #Update and upgrade new system
 #sudo apt update -y && sudo apt upgrade -y
 
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --port)
+            PORT="$2"
+            shift 2
+            ;;
+        --user)
+            USERNAME="$2"
+            shift 2
+            ;;
+        *)
+            echo "Неизвестный параметр: $1"
+            exit 1
+            ;;
+    esac
+done
+
 #Change default OpenSSH port to custom 10122 port-ssh
-FILE="/etc/ssh/sshd_config"
-sed -i 's/^#\?Port .*$/Port 10122/' "$FILE"
+#FILE="/etc/ssh/sshd_config"
+#sed -i 's/^#\?Port .*$/Port 10122/' "$FILE"
