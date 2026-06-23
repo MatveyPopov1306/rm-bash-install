2672c8c2d40e3e9cd82d5625f81cf00ab267cbad
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 19:08:52 +0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/updatevm.sh.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/updatevm.sh)
