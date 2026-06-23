f736890ff8bda5eb3aa8027b37c66d9ede369b90
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 19:37:59 +0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -6,3 +6,5 @@ bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-instal
 bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/change_default-ssh.sh)
 #Disable password authentification, allow only using ssh-key
 bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/disable_passwordauth.sh)
+#Disable server ping
+bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/disable_ping_server.sh)
