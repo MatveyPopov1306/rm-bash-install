f2d8045598589f76c64736ae82d346794edfd6a1
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 16:49:51 +0300

    Update basic-prepare.sh

--- basic-prepare.sh
+++ basic-prepare.sh
@@ -3,9 +3,17 @@
 #Update and upgrade new system
 #sudo apt update -y && sudo apt upgrade -y
 
-echo "$1"  # --port
-echo "$2"  # 10122
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --port)
+            PORT="$2"
+            shift 2
+            ;;
+    esac
+done
 
+echo "$1"
+echo "$2"
 #Change default OpenSSH port to custom 10122 port-ssh
 #FILE="/etc/ssh/sshd_config"
 #sed -i 's/^#\?Port .*$/Port 10122/' "$FILE"
