6c0fc60393e971067966018c49e7efca7114ad5e
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 17:35:06 +0300

    Update changetest.sh

--- changetest.sh
+++ changetest.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
-FILE="/opt/test/test.txt"
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+RESET='\e[0m'
 
-sed -i '/^changevalue=/ s/=.*/=0/' "$FILE"
+#Update and upgrade new system
+#sudo apt update -y && sudo apt upgrade -y
+#echo "Hello, world"
+
+#Default value of OpenSSH port
+PORT="22" 
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --port)
+            PORT="$2"
+            shift 2
+            ;;
+        *)
+            echo -e "An unknown parameter was passed: $1"
+            echo -e "${RED}[ERROR]${RESET} installation was cancelled"
+            exit 1
+            ;;
+    esac
+done
+
+echo "$PORT"
+echo -e "${GREEN}[OK]${RESET} Установка завершена"
+
+#Change default OpenSSH port to custom 10122 port-ssh
+#FILE="/etc/ssh/sshd_config"
+#sed -i 's/^#\?Port .*$/Port 10122/' "$FILE"
