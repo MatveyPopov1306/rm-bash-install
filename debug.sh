56097e5c4b5ad2d1d490435fca1d002067756e10
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 19:20:48 +0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -4,3 +4,5 @@
 bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/updatevm.sh)
 #Change default ssh port to custom
 bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/change_default-ssh.sh)
+#Disable password authentification, allow only using ssh-key
+bash <(curl -Ls https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/disable_passwordauth.sh)
