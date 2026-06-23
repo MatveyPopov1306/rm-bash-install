46429c53fad509a7b7bdf76b97c79a93122c4c64
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 20:13:30 +0300

    Update ufw_config.sh

--- ufw_config.sh
+++ ufw_config.sh
@@ -7,15 +7,15 @@ YELLOW='\e[33m'
 BLUE='\e[34m'
 RESET='\e[0m'
 
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
+sudo ufw default deny incoming > /dev/null 2>&1
+sudo ufw default allow outgoing > /dev/null 2>&1
 
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 443/udp
-sudo ufw allow 10122/tcp
+sudo ufw allow 80/tcp > /dev/null 2>&1
+sudo ufw allow 443/tcp > /dev/null 2>&1
+sudo ufw allow 443/udp > /dev/null 2>&1
+sudo ufw allow 10122/tcp > /dev/null 2>&1
 
-sudo ufw enable
+sudo ufw --force enable > /dev/null 2>&1
 
 echo -e "${GREEN}[OK] ${RESET} Firewall was configured and enabled\n"
-sudo ufw status verbose
+#sudo ufw status verbose
