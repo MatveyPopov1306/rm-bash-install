497043992eb94aa898acfdf64aeca1eebb1a0aab
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 17:30:12 +0300

    Update basic-prepare.sh

--- basic-prepare.sh
+++ basic-prepare.sh
@@ -8,7 +8,7 @@ RESET='\e[0m'
 
 #Update and upgrade new system
 #sudo apt update -y && sudo apt upgrade -y
-echo "Hello, world"
+#echo "Hello, world"
 
 #Default value of OpenSSH port
 PORT="22" 
@@ -29,7 +29,6 @@ done
 
 echo "$PORT"
 echo -e "${GREEN}[OK]${RESET} Установка завершена"
-echo "Обычный текст"
 
 #Change default OpenSSH port to custom 10122 port-ssh
 #FILE="/etc/ssh/sshd_config"
