fdddfd91929e4b030dc27f830e67f18e9ef8b5df
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 17:15:10 +0300

    Update basic-prepare.sh

--- basic-prepare.sh
+++ basic-prepare.sh
@@ -4,32 +4,15 @@
 #sudo apt update -y && sudo apt upgrade -y
 echo "Hello, world"
 
-PORT="22"
-USERNAME="root"
-TIMEZONE="UTC"
+#Default value of OpenSSH port
+PORT="22" 
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        install|update|remove)
-            ACTION="$1"
-            shift
-            ;;
-
         --port)
             PORT="$2"
             shift 2
             ;;
-
-        --user)
-            USERNAME="$2"
-            shift 2
-            ;;
-
-        --timezone)
-            TIMEZONE="$2"
-            shift 2
-            ;;
-
         *)
             echo "Неизвестный параметр: $1"
             exit 1
@@ -38,8 +21,6 @@ while [[ $# -gt 0 ]]; do
 done
 
 echo "$PORT"
-echo "$USERNAME"
-echo "$TIMEZONE"
 
 #Change default OpenSSH port to custom 10122 port-ssh
 #FILE="/etc/ssh/sshd_config"
