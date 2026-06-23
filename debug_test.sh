7bff27737e8de9303f9e07e56ccbe589ab5dca9b
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 18:03:58 +0300

    Create debug_test.sh

---
+++ debug_test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#Shortcuts of colors
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+RESET='\e[0m'
+
+#Update and upgrade new system
+#sudo apt update -y && sudo apt upgrade -y
+
+PORT="10122" #Default value of OpenSSH port
+
+#Parsing cycle
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --port)
+            PORT="$2"
+            shift 2
+            ;;
+        *)
+            echo -e "An unknown parameter was passed: $1"
+            echo -e "${RED}[ERROR]${RESET} installation was cancelled"
+            exit 1
+            ;;
+    esac
+done
+
+#Change default OpenSSH port to custom 10122 port-ssh
+FILE="/etc/ssh/sshd_config"
+sed -i "s|^#\?Port .*$|Port ${PORT}|" "$FILE"
+
+#Reload daemon to activate new SSH port
+sudo systemctl daemon-reload && sudo systemctl restart ssh
