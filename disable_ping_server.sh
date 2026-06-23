e6c649b2d3cad88617b0fbf64329497732b2dfa0
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-23 19:37:27 +0300

    Create disable_ping_server.sh

---
+++ disable_ping_server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Shortcuts of colors
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+RESET='\e[0m'
+
+FILE="/etc/ufw/before.rules"
+
+# Меняем ACCEPT на DROP в нужных строках
+sed '/# ok icmp codes for INPUT/,/# ok icmp code for FORWARD/ s/ACCEPT/DROP/g' "$FILE"
+sed '/# ok icmp code for FORWARD/,/^#/ s/ACCEPT/DROP/g' "$FILE"
+
+# Добавляем source-quench после комментария
+sed '/# ok icmp codes for INPUT/a -A ufw-before-input -p icmp --icmp-type source-quench -j DROP' "$FILE"
