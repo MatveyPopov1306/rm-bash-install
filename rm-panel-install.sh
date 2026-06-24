7747d3ffd9a1312e3d8f96af25416961b4364e5c
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-06-24 10:35:25 +0300

    Update rm-panel-install.sh

--- rm-panel-install.sh
+++ rm-panel-install.sh
@@ -73,9 +73,45 @@ echo -e "$OK Domain addreses were added to .env file"
 #Start the containers
 docker compose up -d && docker compose logs -f -t
 
+#_________________________________________________________________    REVERSE-PROXY INSTALL    ________________________________________________________________________________
 
+#Install dependencies Socat
+if command -v socat >/dev/null 2>&1; then
+    echo -e "$OK Cron and socat already installed"
+else
+    sudo apt-get install -y cron socat
+fi
 
+#Install acme.sh with custom email
+curl https://get.acme.sh | sh -s email=$EMAIN_ADDR && source ~/.bashrc
 
+#Create a folder for the certificates
+mkdir -p /opt/remnawave/nginx && cd /opt/remnawave/nginx
+
+#Set Let'sencrypt for issue ssl sertificate
+acme.sh --set-default-ca --server letsencrypt
+
+#Issue a certificate
+acme.sh --issue --standalone -d "$PANEL_DOMAIN" --key-file /opt/remnawave/nginx/privkey.key --fullchain-file /opt/remnawave/nginx/fullchain.pem --reloadcmd "docker exec remnawave-nginx nginx -s reload"
+
+#This shows that the certificate is issued. Acme.sh will take care of automatically renewing the certificate every 60 days
+acme.sh --install-cert -d "$PANEL_DOMAIN" --key-file /opt/remnawave/nginx/privkey.key --fullchain-file /opt/remnawave/nginx/fullchain.pem --reloadcmd "docker exec remnawave-nginx nginx -s reload"
+
+nginx_cfg_path="/opt/remnawave/nginx/nginx.conf"
+
+#Create a file called nginx.conf in the /opt/remnawave/nginx directory
+cd /opt/remnawave/nginx && curl -o nginx.conf https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/nginx.conf > /dev/null 2>&1
+
+#Change sample nginx conf and add user's domain
+sed -i "s|^.*server_name REPLACE_WITH_YOUR_DOMAIN;$|    server_name domain.com;|" "$nginx_cfg_path"
+
+#Download and place sample of Docker compose yml
+cd /opt/remnawave/nginx && curl -o nginx.conf https://raw.githubusercontent.com/MatveyPopov1306/rm-bash-install/main/docker-compose.yml > /dev/null 2>&1
+
+#Finish, start the containers
+docker compose up -d && docker compose logs -f -t
+
+#_______________________________________________________________________    SUBSCRIPTION PANEL INSTALL+RR-PROXY    ________________________________________________________________________________
 
 
 
