ac8feb6b53f39b5d19d655402ebb71f5ba65bf38
MatveyPopov1306 <matvey.popov1306@gmail.com>
2026-08-28 13:08:42 +0300

    Create basic-prepare3.sh

---
+++ basic-prepare3.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#Shortcuts of colors
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+RESET='\e[0m'
+
+#Error codes for echo -e
+OK="${GREEN}[OK]${RESET}"
+ERROR="${RED}[ERROR]${RESET}"
+WARNING="${YELLOW}[WARNING]${RESET}"
+
+update_func() {
+
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+
+sudo apt install ufw -y
+sudo apt install fail2ban -y
+sudo apt install speedtest-cli
+
+}
+
+main() {
+
+update_func
+
+}
+
+main
